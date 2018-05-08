e1c9edda952d6541f028b4abbad3bd386ef6c26b
Jags <jags14385@users.noreply.github.com>
2018-05-09 05:31:47 +1000

    Adding spec files

--- clients/bankAPIClient.ts
+++ clients/bankAPIClient.ts
@@ -2,14 +2,16 @@ import { APIClient } from "./apiClient";
 
 export class BankAPIClient extends APIClient {
 
-    saveDetails() {
+    saveDetails(payment_method:string,
+        bank_country_code:string,account_name:string,
+        account_number:string,swift_code:string,aba:string) {
         var jsonData = {
-                "payment_method": "SWIFT",
-                "bank_country_code": "US",
-                "account_name": "John Smith",
-                "account_number": "123",
-                "swift_code": "ICBCUSBJ",
-                "aba": "11122233A"
+                "payment_method": payment_method,
+                "bank_country_code": bank_country_code,
+                "account_name": account_name,
+                "account_number": account_number,
+                "swift_code": swift_code,
+                "aba": aba
         }
         return this.getAPIInstance().post('/',jsonData)
                     .then(response => response.data);
