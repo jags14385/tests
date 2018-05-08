5d1957a3a913ac21e7937bc005f496d543a6d7c9
Jags <jags14385@users.noreply.github.com>
2018-05-09 05:47:39 +1000

    tslint fixes

--- spec/bank.spec.ts
+++ spec/bank.spec.ts
@@ -1,15 +1,15 @@
-import * as APIClient from '../clients/bankAPIClient';
+import * as APIClient from "../clients/bankAPIClient";
 
-describe('Bank API',() => {
-    let bankAPIClient:APIClient.BankAPIClient;
+describe("Bank API", () => {
+    let bankAPIClient: APIClient.BankAPIClient;
 
     beforeEach(async () => {
         bankAPIClient = new APIClient.BankAPIClient();
     });
-    
-    it('should return successful repsonse for valid details',async () => {
-        let response = await bankAPIClient.saveDetails('LOCAL','US','aba',
-                                        '123','ICBCUSBJ','11122233AA');
+
+    it("should return successful repsonse for valid details", async () => {
+        const response = await bankAPIClient.saveDetails("LOCAL", "US", "aba",
+                                        "123", "ICBCUSBJ", "11122233AA");
         console.log(response);
     });
 });
