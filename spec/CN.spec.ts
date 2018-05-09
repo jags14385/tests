5672d20277a0848a8b9e7e46ba604a34f446aa3e
Jagannath Vaikuntham <Jagannath.Vaikuntham@auspost.com.au>
2018-05-09 16:00:01 +1000

    Reasonable specs for CN.

--- spec/CN.spec.ts
+++ spec/CN.spec.ts
@@ -8,84 +8,60 @@ describe("Bank API for China", () => {
         bankAPIClient = new APIClient.BankAPIClient();
     });
 
-    it('should work for bank country code AU for acc number length 6',async () => {
+    it('should work for acc number length 8',async () => {
         const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("LOCAL", "AU", "John Smith",
-                    "112311", "", "", "063182");
+        .saveDetails("LOCAL", "CN", "John Smith",
+                    "11231112", "", "", "");
         expect(response.status).toBe(200);
         expect(response.data).toEqual({ success: "Bank details saved" });
     });
 
-    it('should work for account number of length 8',async () => {
+    it('should thow error for account number of length less than 8',async () => {
         const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("LOCAL", "AU", "John Smith",
-                    "11231123", "", "", "063182");
-        expect(response.status).toBe(200);
-        expect(response.data).toEqual({ success: "Bank details saved" });
-    });
-
-    it('should work for account number of length 9',async () => {
-        const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("LOCAL", "AU", "John Smith",
-                    "112311231", "", "", "063182");
-        expect(response.status).toBe(200);
-        expect(response.data).toEqual({ success: "Bank details saved" });
-    });
-
-    it('should work for bank country code AU',async () => {
-        const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("LOCAL", "AU", "John Smith",
-                    "11231123", "", "", "063182");
-        expect(response.status).toBe(200);
-        expect(response.data).toEqual({ success: "Bank details saved" });
-    });
-
-    it('should throw error when bsb is not supplied',async () => {
-        const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("LOCAL", "AU", "John Smith",
-                    "123311", "", "","");
+        .saveDetails("LOCAL", "CN", "John Smith",
+                    "12331", "", "","");
         expect(response.status).toBe(400);
-        expect(response.data).toEqual({ error: "'bsb' is required when bank country code is 'AU'"});
+        expect(response.data).toEqual({ error: "Length of account_number should be between 8 and 20 when bank_country_code is 'CN'" });
     });
 
-    it('should throw error when bsb length not equal to 6',async () => {
+    it('should thow error for account number of length greater than 20',async () => {
         const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("LOCAL", "AU", "John Smith",
-                    "123311", "", "","0456A");
+        .saveDetails("LOCAL", "CN", "John Smith",
+                    "123311233112331123311", "", "","");
         expect(response.status).toBe(400);
-        expect(response.data).toEqual({ error: "Length of 'bsb' should be 6"});
+        expect(response.data).toEqual({ error: "Length of account_number should be between 8 and 20 when bank_country_code is 'CN'" });
+    });
+
+    it('should work for account number of length 15',async () => {
+        const response: AxiosResponse<any> = await bankAPIClient
+        .saveDetails("LOCAL", "CN", "John Smith",
+                    "1123112312345", "", "", "");
+        expect(response.status).toBe(200);
+        expect(response.data).toEqual({ success: "Bank details saved" });
+    });
+
+    it('should work for account number of length 20',async () => {
+        const response: AxiosResponse<any> = await bankAPIClient
+        .saveDetails("LOCAL", "CN", "John Smith",
+                    "12345123451234512345", "", "", "");
+        expect(response.status).toBe(200);
+        expect(response.data).toEqual({ success: "Bank details saved" });
     });
 
     it('should successfully save details for valid swift code',async () => {
         const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("SWIFT", "AU", "John Smith",
-                    "123311", "CTBAAU2S7)", "","063182");
+        .saveDetails("SWIFT", "CN", "John Smith",
+                    "12341234", "CTBACNCD", "","");
         expect(response.status).toBe(200);
         expect(response.data).toEqual({ success: "Bank details saved" });
     });
 
     it('should thow error for invalid swift code',async () => {
         const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("SWIFT", "AU", "John Smith",
-                    "123311", "CTBAJK2S7)", "","063182");
+        .saveDetails("SWIFT", "CN", "John Smith",
+                    "12341234", "CTBAAU2S7)", "","");
         expect(response.status).toBe(400);
-        expect(response.data).toEqual({ error: "The swift code is not valid for the given bank country code: AU" });
-    });
-
-    it('should thow error for account number length less than 6',async () => {
-        const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("SWIFT", "AU", "John Smith",
-                    "12331", "CTBAJK2S7)", "","063182");
-        expect(response.status).toBe(400);
-        expect(response.data).toEqual({ error: "Length of account_number should be between 6 and 9 when bank_country_code is 'AU'" });
-    });
-
-    it('should thow error for account number length greater than 9',async () => {
-        const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("SWIFT", "AU", "John Smith",
-                    "1233112331", "CTBAJK2S7)", "","063182");
-        expect(response.status).toBe(400);
-        expect(response.data).toEqual({ error: "Length of account_number should be between 6 and 9 when bank_country_code is 'AU'" });
+        expect(response.data).toEqual({ error: "The swift code is not valid for the given bank country code: CN" });
     });
 
 });
