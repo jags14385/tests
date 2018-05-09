a2043e3def439d3051f0d5fed48aae0a6f7bb830
Jagannath Vaikuntham <Jagannath.Vaikuntham@auspost.com.au>
2018-05-09 16:13:47 +1000

    Adding swift code spec

---
+++ spec/swift_code.spec.ts
@@ -0,0 +1,50 @@
+import * as APIClient from "../clients/bankAPIClient";
+import { AxiosResponse } from "axios";
+
+describe("Bank API", () => {
+    let bankAPIClient: APIClient.BankAPIClient;
+
+    beforeEach(async () => {
+        bankAPIClient = new APIClient.BankAPIClient();
+    });
+
+    it('should successfully process for valid swift code of length 8',async () => {
+        const response: AxiosResponse<any> = await bankAPIClient
+        .saveDetails("SWIFT", "US", "John Smith",
+                    "123", "ABCDUSIO", "11122277A", "");
+        expect(response.status).toBe(200);
+        expect(response.data).toEqual({ success: "Bank details saved" });
+    });
+
+    it('should successfully process for valid swift code of length 11',async () => {
+        const response: AxiosResponse<any> = await bankAPIClient
+        .saveDetails("SWIFT", "US", "John Smith",
+                    "123", "ABCDUSIOIOI", "11122277A", "");
+        expect(response.status).toBe(200);
+        expect(response.data).toEqual({ success: "Bank details saved" });
+    });
+
+    it('should throw error for swift code of length 9',async () => {
+        const response: AxiosResponse<any> = await bankAPIClient
+        .saveDetails("SWIFT", "US", "John Smith",
+                    "123", "ABCDUSIO1", "11122277A", "");
+        expect(response.status).toBe(400);
+        expect(response.data).toEqual({ error: "Length of 'swift_code' should be either 8 or 11" });    
+    });
+
+    it('should throw error for swift code of length 7',async () => {
+        const response: AxiosResponse<any> = await bankAPIClient
+        .saveDetails("SWIFT", "US", "John Smith",
+                    "123", "ABCDUSI", "11122277A", "");
+        expect(response.status).toBe(400);
+        expect(response.data).toEqual({ error: "Length of 'swift_code' should be either 8 or 11" });
+    });
+
+    it('should throw error for swift code of length 12',async () => {
+        const response: AxiosResponse<any> = await bankAPIClient
+        .saveDetails("SWIFT", "US", "John Smith",
+                    "123", "ABCDUSIO12IO", "11122277A", "");
+        expect(response.status).toBe(400);
+        expect(response.data).toEqual({ error: "Length of 'swift_code' should be either 8 or 11" });    
+    });
+});
