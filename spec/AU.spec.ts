12c72b89745dbbad9a36ea5a40215e935cf7a2a5
Jagannath Vaikuntham <Jagannath.Vaikuntham@auspost.com.au>
2018-05-09 15:23:27 +1000

    Reasonable AU specs

--- spec/AU.spec.ts
+++ spec/AU.spec.ts
@@ -14,13 +14,29 @@ describe("Bank API for AU", () => {
                     "11231123", "", "", "063182");
         expect(response.status).toBe(200);
         expect(response.data).toEqual({ success: "Bank details saved" });
-
     });
 
     it('should throw error when bsb is not supplied',async () => {
         const response: AxiosResponse<any> = await bankAPIClient
         .saveDetails("LOCAL", "AU", "John Smith",
                     "123311", "", "","");
-        expect(response.status).toBe(1200);
+        expect(response.status).toBe(400);
+        expect(response.data).toEqual({ error: "'bsb' is required when bank country code is 'AU'"});
     });
+
+    it('should throw error when bsb length not equal to 6',async () => {
+        const response: AxiosResponse<any> = await bankAPIClient
+        .saveDetails("LOCAL", "AU", "John Smith",
+                    "123311", "", "","0456A");
+        expect(response.status).toBe(400);
+        expect(response.data).toEqual({ error: "Length of 'bsb' should be 6"});
+    });
+
+    it('should successfully save details for valid swift code',async () => {
+        const response: AxiosResponse<any> = await bankAPIClient
+        .saveDetails("SWIFT", "AU", "John Smith",
+                    "123311", "CTBAAU2S7)", "","063182");
+        expect(response.status).toBe(200);
+    });
+
 });
