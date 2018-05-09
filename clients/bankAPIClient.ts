d2f132effa5af45ff2553456390645e9789a6a6b
Jagannath Vaikuntham <Jagannath.Vaikuntham@auspost.com.au>
2018-05-09 16:55:24 +1000

    Commenting the console.log debug stmts.

--- clients/bankAPIClient.ts
+++ clients/bankAPIClient.ts
@@ -28,8 +28,8 @@ export class BankAPIClient extends APIClient {
                 if (error.response) {
                     // The request was made and the server responded with a status code
                     // that falls out of the range of 2xx
-                    console.log(error.response.data);
-                    console.log(error.response.status);
+                    // console.log(error.response.data);
+                    // console.log(error.response.status);
                     // console.log(error.response);
                   }
                 return error.response;
