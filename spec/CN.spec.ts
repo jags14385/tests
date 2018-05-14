17dc47c80783760218a9fc981c21be02e133a0c7
Jagannath Vaikuntham <Jagannath.Vaikuntham@auspost.com.au>
2018-05-14 14:53:05 +1000

    Using constants

--- spec/CN.spec.ts
+++ spec/CN.spec.ts
@@ -1,5 +1,6 @@
 import { AxiosResponse } from "axios";
 import * as APIClient from "../clients/bankAPIClient";
+import {SWIFT_PAYMENT_MODE, LOCAL_PAYMENT_MODE} from "../utils/Constants"
 
 describe("Bank API for China", () => {
     let bankAPIClient: APIClient.BankAPIClient;
@@ -10,7 +11,7 @@ describe("Bank API for China", () => {
 
     it("should work for acc number length 8", async () => {
         const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("LOCAL", "CN", "John Smith",
+        .saveDetails(LOCAL_PAYMENT_MODE, "CN", "John Smith",
                     "11231112", "", "", "");
         expect(response.status).toBe(200);
         expect(response.data).toEqual({ success: "Bank details saved" });
@@ -18,7 +19,7 @@ describe("Bank API for China", () => {
 
     it("should thow error for account number of length less than 8", async () => {
         const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("LOCAL", "CN", "John Smith",
+        .saveDetails(LOCAL_PAYMENT_MODE, "CN", "John Smith",
                     "12331", "", "", "");
         expect(response.status).toBe(400);
         expect(response.data).toEqual({ error: "Length of account_number should be between 8 and 20 when bank_country_code is 'CN'" });
@@ -26,7 +27,7 @@ describe("Bank API for China", () => {
 
     it("should thow error for account number of length greater than 20", async () => {
         const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("LOCAL", "CN", "John Smith",
+        .saveDetails(LOCAL_PAYMENT_MODE, "CN", "John Smith",
                     "123311233112331123311", "", "", "");
         expect(response.status).toBe(400);
         expect(response.data).toEqual({ error: "Length of account_number should be between 8 and 20 when bank_country_code is 'CN'" });
@@ -34,7 +35,7 @@ describe("Bank API for China", () => {
 
     it("should work for account number of length 15", async () => {
         const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("LOCAL", "CN", "John Smith",
+        .saveDetails(LOCAL_PAYMENT_MODE, "CN", "John Smith",
                     "1123112312345", "", "", "");
         expect(response.status).toBe(200);
         expect(response.data).toEqual({ success: "Bank details saved" });
@@ -42,7 +43,7 @@ describe("Bank API for China", () => {
 
     it("should work for account number of length 20", async () => {
         const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("LOCAL", "CN", "John Smith",
+        .saveDetails(LOCAL_PAYMENT_MODE, "CN", "John Smith",
                     "12345123451234512345", "", "", "");
         expect(response.status).toBe(200);
         expect(response.data).toEqual({ success: "Bank details saved" });
@@ -50,7 +51,7 @@ describe("Bank API for China", () => {
 
     it("should successfully save details for valid swift code", async () => {
         const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("SWIFT", "CN", "John Smith",
+        .saveDetails(SWIFT_PAYMENT_MODE, "CN", "John Smith",
                     "12341234", "CTBACNCD", "", "");
         expect(response.status).toBe(200);
         expect(response.data).toEqual({ success: "Bank details saved" });
@@ -58,7 +59,7 @@ describe("Bank API for China", () => {
 
     it("should thow error for invalid swift code", async () => {
         const response: AxiosResponse<any> = await bankAPIClient
-        .saveDetails("SWIFT", "CN", "John Smith",
+        .saveDetails(SWIFT_PAYMENT_MODE, "CN", "John Smith",
                     "12341234", "CTBAAU2S7)", "", "");
         expect(response.status).toBe(400);
         expect(response.data).toEqual({ error: "The swift code is not valid for the given bank country code: CN" });
---
+++ utils/Constants.ts
@@ -0,0 +1,2 @@
+export const SWIFT_PAYMENT_MODE = "SWIFT";
+export const LOCAL_PAYMENT_MODE = "LOCAL";
